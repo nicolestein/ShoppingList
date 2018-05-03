c371ddf29a15853fee33b94f6ddca30b7cb0d78c
Nicole I Stein <ns60549@srcs.loc>
2018-05-03 11:24:11 -0700

    fixing

--- DynamicListApp.js
+++ DynamicListApp.js
@@ -1,25 +1,25 @@
 var submitbutton = document.getElementById("submit");
-submitbutton.addEventListener("click", () => { addItem();
-                                             });
+submitbutton.addEventListener("click", () => {
+    additem();
+});
 var list = document.getElementsByTagName("ul")[0];
+
 function additem() {
-        var input = document.getElementById("item").value;
-        var li = document.createElement("li");
-        var textcontent = document.createElement("empty");
-        var deletebutton = document.createElement("button");
+    var input = document.getElementById("item").value;
+    var li = document.createElement("li");
+    var textcontent = document.createElement("empty");
+    var deletebutton = document.createElement("button");
 
-          document.getElementById("item").value = ""
+    document.getElementById("item").value = "";
 
-       deletebutton.textContent = "remove item";
-       deletebutton.className = "delete"
-       deletebutton.addEventListener("click", () => { var target = deletebutton.parentElement;
-                                                    target.remove();
-                                                    });
-       text.content.textContent = input + "";
-       li.appendChild(textcontent);
-       li.appendChild(deletebutton);
-       list.appendChild(li);
-
-
-
-    }
+    deletebutton.textContent = "remove item";
+    deletebutton.className = "delete";
+    deletebutton.addEventListener("click", () => {
+        var target = deletebutton.parentElement;
+        target.remove();
+    });
+    textcontent.textContent = input + "";
+    li.appendChild(textcontent);
+    li.appendChild(deletebutton);
+    list.appendChild(li);
+}
