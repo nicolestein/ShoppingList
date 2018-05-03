501dd5b86db017774a4ce2b9e551a275d14e68fc
Olivia Hardwidge <olivia@Olivias-Air.hsd1.ca.comcast.net>
2018-05-02 21:29:34 -0700

    commit fix lost files

--- DynamicListApp.js
+++ DynamicListApp.js
@@ -1,11 +1,25 @@
 var submitbutton = document.getElementById("submit");
-submitbutton.addEventListener("click", () => additem(););
+submitbutton.addEventListener("click", () => { addItem();
+                                             });
 var list = document.getElementsByTagName("ul")[0];
-   function additem() {
-        var imput = document.getElementById("item").value;
+function additem() {
+        var input = document.getElementById("item").value;
         var li = document.createElement("li");
         var textcontent = document.createElement("empty");
         var deletebutton = document.createElement("button");
+
+          document.getElementById("item").value = ""
+
+       deletebutton.textContent = "remove item";
+       deletebutton.className = "delete"
+       deletebutton.addEventListener("click", () => { var target = deletebutton.parentElement;
+                                                    target.remove();
+                                                    });
+       text.content.textContent = input + "";
+       li.appendChild(textcontent);
+       li.appendChild(deletebutton);
+       list.appendChild(li);
+
 
 
     }
